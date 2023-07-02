fb25c11650f6d9b58699191bd40e8f2b42c1c04d
Egor <nakonectlichika@mail.ru>
2023-07-02 20:31:41 +0300

    Blanks for Account and Bills

--- frontend/src/Account.jsx
+++ frontend/src/Account.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import React from "react"
 import BillCard from "./BillCard"
-import { Box } from "@mui/material"
+import { Box, Dialog, DialogTitle, Button, Typography, Input, FormControl, RadioGroup, FormControlLabel, Radio, FormLabel} from "@mui/material"
 import axios from "axios"
 import Header from "./Header"
 
@@ -11,6 +11,10 @@ export default function Account(props){
     const[accountInfo, setAccountInfo] = useState()
     const[isLoaded, setLoaded] = useState(false)
     const {id} = useParams()
+    const[popUpState, setPopUpState] = useState(false)
+    const[title, setTitle] = useState("")
+    const[description, setDescription] = useState("")
+    const[chosenType, setChosenType] = useState(0)
 
     const axiosInstance = axios.create({
         withCredentials: true
@@ -25,16 +29,61 @@ export default function Account(props){
         })
     }
 
+    function changePopUpState(){
+        setPopUpState(!popUpState)
+    }
+
+    function typeChanged(event){
+        setChosenType(event.target.value)
+    }
+
     useEffect(()=>{
         if(!isLoaded){
             produceAccountBillsLoad()
         }
     }, [])
 
+    function updateDescription(event){
+        setDescription(event.target.value)
+    }
+
+    function updateTitle(event){
+        setTitle(event.target.value)
+    }
+
     return(
         <React.Fragment>
-          <Box className = "dashboardContainer" sx={{width:"100%", height:"100vh"}}>
+
+<Dialog onClose={changePopUpState} open={popUpState}>          
+                <DialogTitle>Создание траты</DialogTitle>
+                <Box width={"60vw"} height={"50vh"}>
+                    <Box display={"flex"} flexDirection={"column"}> 
+                        <Input id="titleInput" onChange={updateTitle} placeholder="Введите название траты"/>
+                        <Input id="descriptionInput" onChange={updateDescription} placeholder="Введите описание траты"/>
+
+                                            <FormControl>
+                        <FormLabel >Тип</FormLabel>
+                        <RadioGroup
+                            value={chosenType}
+                            onChange={typeChanged}
+                            row
+                            aria-labelledby="demo-row-radio-buttons-group-label"
+                            name="row-radio-buttons-group"
+                        >
+                            <FormControlLabel value={0} control={<Radio />} label="Авто" />
+                            <FormControlLabel value={1} control={<Radio />} label="Процент" />
+                            <FormControlLabel value={2} control={<Radio />} label="Вручную" />
+                        </RadioGroup>
+                        </FormControl>
+
+                    </Box>
+                        <Button >Создать трату</Button>
+                </Box>
+            </Dialog>
+
+          <Box className = "accountContainer" sx={{width:"100%", height:"100vh"}}>
             <Header updateAuthStatus={props.updateAuthStatus}/>
+            <Button onClick={changePopUpState}>Создать трату</Button>
             {isLoaded?
             <React.Fragment>
                 {accountInfo.bills.map(obj=>{
--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link} from "react-router-dom";
 import axios from "axios";
-import { Box, Button, Card, CardContent, Typography} from "@mui/material";
+import { Box, Button, Card, CardContent, Typography, Dialog, DialogTitle, Input, Avatar} from "@mui/material";
 import React from "react";
 import Account from "./Account";
 import AccountCard from "./AccountCard";
@@ -16,6 +16,12 @@ export default function Dashboard(props){
     //const[debts, setDebts] = useState()
     const[isLoaded, setLoaded] = useState(false)
     const[userInfo, setUserInfo] = useState([])
+    const[popUpState, setPopUpState] = useState(false)
+    const[usersToAdd, setUsersToAdd] = useState([])
+    const[lastUserInfo, setLastUserInfo] = useState(null)
+    const[title, setTitle] = useState("")
+    const[description, setDescription] = useState("")
+    const[nextUserContainerEnabled, setNextUserContainerEnabled] = useState(false)
 
     function loadUserInfo(){
         axiosInstance.get("http://localhost:8080/api/getUserInfo")
@@ -40,16 +46,108 @@ export default function Dashboard(props){
         )
     }
 
+    function changePopUpState(){
+        if(popUpState){
+            setNextUserContainerEnabled(false)
+        }
+        setPopUpState(!popUpState)
+    }
+
+    function fetchUser(event){
+        if(usersToAdd.filter(user => user.userId == event.target.value).length === 0){
+            if(event.target.value!=""){
+            axiosInstance.get("http://localhost:8080/api/fetchUser", {params:{
+                userId: event.target.value
+            }}).then(res=>{
+                setLastUserInfo(res.data)
+                setNextUserContainerEnabled(true)
+            }).catch(()=>setNextUserContainerEnabled(false))
+        }
+        else{
+            setNextUserContainerEnabled(false)
+        }
+    }
+    else{
+        setNextUserContainerEnabled(false)
+    }
+    }
+
+    function createAccount(){
+        var userInAccount = ""
+        for(let i = 0; i<usersToAdd.length; i++){
+            if(i!=usersToAdd.length-1)
+                userInAccount+=usersToAdd[i].userId+","
+            else
+                userInAccount+=usersToAdd[i].userId
+        }
+
+
+        axiosInstance.post("http://localhost:8080/api/createAccount", null, { params:{
+            title: title,
+            description: description,
+            users: userInAccount
+        }})
+    }
+
+    function addUser(){
+        var c = usersToAdd
+        c.push(lastUserInfo)
+        setUsersToAdd(c)
+        setNextUserContainerEnabled(false)
+    }
+
+    function updateDescription(event){
+        setDescription(event.target.value)
+    }
+
+    function updateTitle(event){
+        setTitle(event.target.value)
+    }
+
     useEffect(()=>{
         if(!isLoaded){
                 loadUserInfo()
         }
-    },[isLoaded])
+    },[isLoaded, nextUserContainerEnabled])
     return(
         <React.Fragment>
+             <Dialog onClose={changePopUpState} open={popUpState}>          
+                <DialogTitle>Создание счёта</DialogTitle>
+                <Box width={"60vw"} height={"50vh"}>
+                    <Box display={"flex"} flexDirection={"column"}> 
+                        <Input id="titleInput" onChange={updateTitle} placeholder="Введите название счёта"/>
+                        <Input id="descriptionInput" onChange={updateDescription} placeholder="Введите описание счёта"/>
+                        <Input id="idInput" onChange={fetchUser} placeholder="Введите ID пользователя"/>
+                    </Box>
+                    {nextUserContainerEnabled?
+                    <Box onClick={addUser} display={"flex"} flexDirection={"row"} justifyContent={"space-between"}>
+                        <Avatar src={lastUserInfo.imgLink}/>
+                        <Typography>{lastUserInfo.name}</Typography>
+                        <Typography>{lastUserInfo.surname}</Typography>
+                    </Box>:
+                        null}
+                        {usersToAdd.length!=0?
+                                                <React.Fragment>
+                        <Typography>Пользователи в счёте</Typography>
+                            {usersToAdd.map(user=>{
+                                return(
+                                    <Box key={user.userId} display={"flex"} flexDirection={"row"} justifyContent={"space-between"}>
+                                    <Avatar src={user.imgLink}/>
+                                    <Typography>{user.name}</Typography>
+                                    <Typography>{user.surname}</Typography>
+                                </Box>
+                                )
+                            })}
+                        </React.Fragment>
+                        :null}
+                        <Button onClick={createAccount}>Создать счёт</Button>
+                </Box>
+            </Dialog>
+
              {isLoaded?
          <Box className = "dashboardContainer" sx={{width:"100%", height:"100vh"}}>
              <Header updateAuthStatus={props.updateAuthStatus}/>
+             <Button onClick={changePopUpState}>Создать счёт</Button>
                  <Card sx={{borderRadius: "20px", margin:'2%', boxShadow:'0 2px 5px rgba(0,0,0,0.5)'}}>
                      <CardContent sx={{display:'flex', alignItems:'center'}}>
                      {userInfo.totalDebt === 0?
@@ -64,9 +162,7 @@ export default function Dashboard(props){
                  {userInfo.accounts.map(obj=>{
                 
                      return (
-                        // <Link to ={"accouhdfghdfhnt"}>
                             <AccountCard key={obj.accountId} account={obj}/>
-                        // </Link>
                         )
                     
                  })}
@@ -74,6 +170,5 @@ export default function Dashboard(props){
          </Box>:
          null}
         </React.Fragment>
-        // </React.Fragment>
     )
 }
