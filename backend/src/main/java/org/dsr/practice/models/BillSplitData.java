db07fe5d12c2f885203738cc46da8342fd9b3f68
Egor <nakonectlichika@mail.ru>
2023-07-03 17:42:26 +0300

    Price update

---
+++ backend/src/main/java/org/dsr/practice/models/BillSplitData.java
@@ -0,0 +1,71 @@
+package org.dsr.practice.models;
+
+import org.dsr.practice.models.Pairs;
+import org.dsr.practice.utils.PriceConverter;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+public class BillSplitData {
+    private String title;
+    private String description;
+    private Long fullPrice;
+    private Long accountId;
+    private Map<Long, Long> usersMap;
+
+    private int type;
+
+    public BillSplitData() {
+    }
+
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getFullPrice() {
+        return fullPrice;
+    }
+
+    public void setFullPrice(Double fullPrice) {
+        this.fullPrice = Double.valueOf(fullPrice*100).longValue();
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public Map<Long, Long> getUsersMap() {
+        return usersMap;
+    }
+
+    public void setUsersMap(List<Pairs> usersMap) {
+
+        this.usersMap = PriceConverter.convert(usersMap);
+    }
+}
---
+++ backend/src/main/java/org/dsr/practice/models/Pairs.java
@@ -0,0 +1,26 @@
+package org.dsr.practice.models;
+
+public class Pairs {
+
+    private Long userId;
+    private Double debt;
+
+    public Pairs() {
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Double getDebt() {
+        return debt;
+    }
+
+    public void setDebt(Double debt) {
+        this.debt = debt;
+    }
+}
